8ad5033aced8c179af572dfcf6e14574d32d552e
Sarfaraz <sahmed@merative.com>
2024-01-15 16:45:43 -0500

    some changes

--- src/Example.java
+++ src/Example.java
@@ -15,7 +15,7 @@ public class Example {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		System.out.println("Test 1");
 	}
 
 }
